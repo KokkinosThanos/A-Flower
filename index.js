57268b92e464c9b129e0c4cda12a941dcc7ab142
Thanos <thanos@example.com>
2021-03-30 16:25:17 +0300

    Changed landscape...

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // ----------------------------------------------------
 //  Style the <body> element...
 
@@ -18,14 +20,15 @@ const sky = document.getElementById('sky');
 const skyStyle = {
     width: '100%',
     height: '70%',
-    backgroundColor: 'blue',
+    backgroundColor: '',
     position:'absolute',
     top: '0',
     left: '0',
     backgroundImage: 'url(SkyReady.png)',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
-    backgroundSize: 'cover'
+    backgroundSize: 'cover',
+    animation: 'SkyColors 30s infinite'
 }
 
 Object.assign(sky.style,skyStyle);
@@ -39,10 +42,10 @@ const sun = document.getElementById('sun');
 const sunStyle = {
     width: '50px',
     height: '50px',
-    backgroundColor: '',
+    backgroundColor: 'yellow',
     position: 'absolute',
     borderRadius: '50%',
-    animation: 'banner 30s infinite'
+    animation: 'SunMovement 30s infinite'
 }
 
 Object.assign(sun.style,sunStyle);
@@ -58,8 +61,7 @@ sun.innerHTML = `<canvas id='sunElement' width="100px" height="100px" style= " p
                 </canvas>`;
 
 
-let theSun = sunElement.getContext('2d');
-
+const theSun = sunElement.getContext('2d');
 theSun.beginPath();
 theSun.arc(50,50,25,0,2*Math.PI);
 theSun.fillStyle = "rgba(251, 247, 6, 0.5)";
@@ -67,37 +69,71 @@ theSun.strokeStyle = "rgba(251, 247, 6, 0.5)";
 theSun.stroke();
 theSun.fill();
 
-let sunbeam1 = sunElement.getContext('2d');
 
+const sunbeam1 = sunElement.getContext('2d');
 sunbeam1.beginPath();
 sunbeam1.moveTo(50, 80);
 sunbeam1.lineTo(50, 95);
 sunbeam1.strokeStyle = "rgba(251, 247, 6, 0.5)";
 sunbeam1.stroke();
 
-let sunbeam2 = sunElement.getContext('2d');
 
+const sunbeam2 = sunElement.getContext('2d');
 sunbeam2.beginPath();
 sunbeam2.moveTo(50, 20);
 sunbeam2.lineTo(50, 5);
 sunbeam2.strokeStyle = "rgba(251, 247, 6, 0.5)";
 sunbeam2.stroke();
 
-let sunbeam3 = sunElement.getContext('2d');
 
+const sunbeam3 = sunElement.getContext('2d');
 sunbeam3.beginPath();
 sunbeam3.moveTo(80, 50);
 sunbeam3.lineTo(95, 50);
 sunbeam3.strokeStyle = "rgba(251, 247, 6, 0.5)";
 sunbeam3.stroke();
 
-let sunbeam4 = sunElement.getContext('2d');
 
+const sunbeam4 = sunElement.getContext('2d');
 sunbeam4.beginPath();
 sunbeam4.moveTo(20, 50);
 sunbeam4.lineTo(5, 50);
 sunbeam4.strokeStyle = "rgba(251, 247, 6, 0.5)";
 sunbeam4.stroke();
+
+
+const sunbeam5 = sunElement.getContext('2d');
+sunbeam5.beginPath();
+sunbeam5.moveTo(20, 80);
+sunbeam5.lineTo(5, 95);
+sunbeam5.strokeStyle = "rgba(251, 247, 6, 0.5)";
+sunbeam5.stroke();
+
+
+const sunbeam6 = sunElement.getContext('2d');
+sunbeam6.beginPath();
+sunbeam6.moveTo(80, 20);
+sunbeam6.lineTo(95, 5);
+sunbeam6.strokeStyle = "rgba(251, 247, 6, 0.5)";
+sunbeam6.stroke();
+
+
+const sunbeam7 = sunElement.getContext('2d');
+sunbeam7.beginPath();
+sunbeam7.moveTo(20, 20);
+sunbeam7.lineTo(5, 5);
+sunbeam7.strokeStyle = "rgba(251, 247, 6, 0.5)";
+sunbeam7.stroke();
+
+
+const sunbeam8 = sunElement.getContext('2d');
+sunbeam8.beginPath();
+sunbeam8.moveTo(80, 80);
+sunbeam8.lineTo(95, 95);
+sunbeam8.strokeStyle = "rgba(251, 247, 6, 0.5)";
+sunbeam8.stroke();
+
+
 
 
 
@@ -113,19 +149,21 @@ const growndStyle = {
     position: 'absolute',
     left:'0',
     bottom:'0',
-    backgroundImage: 'url(groundTexture.png)',
-    backgroundRepeat: 'repeat',
+    backgroundImage: 'url(Grass.png)',
+    backgroundRepeat: 'no-repeat',
     backgroundPosition: '',
-    backgroundSize: ''
+    backgroundSize: 'cover'
 };
 
 Object.assign(grownd.style, growndStyle);
+
+
 
 // -----------------------------------------------------
 //  Create the Flower element...
 
 
-grownd.innerHTML = "<div id='flowerElement'></div>";
+grownd.innerHTML += "<div id='flowerElement'></div>";
 const flower = document.getElementById('flowerElement');
 
 const flowerStyle = {
@@ -133,13 +171,12 @@ const flowerStyle = {
     height: '500px',
     backgroundColor: '',
     position: 'absolute',
-    left:'50%',
-    top:'-325px',
-    transform: 'translate(-50%, 0)'
+    left:'65%',
+    top:'0',
+    transform: 'translate(-50%, -65%)'
 }
 
 Object.assign(flower.style, flowerStyle);
-
 
 
 
@@ -157,117 +194,180 @@ const flowerDraw = document.getElementById('flowerDraw');
 
 // Flowers center
 
-let ctx = flowerDraw.getContext('2d');
+const fl0 = flowerDraw.getContext('2d');
 
-ctx.beginPath();
-ctx.arc(150,150,35,0,2*Math.PI);
-ctx.fillStyle = "black";
-ctx.strokeStyle = "purple";
-ctx.stroke();
-ctx.fill();
-
-// Flowers petalo
-
-let ctx1 = flowerDraw.getContext('2d');
-
-ctx1.beginPath();
-ctx1.arc(90,150,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
-
+fl0.beginPath();
+fl0.arc(150,150,35,0,2*Math.PI);
+fl0.fillStyle = "rgb(219, 212, 197)";
+fl0.strokeStyle = "black";
+fl0.lineWidth = 1;
+fl0.stroke();
+fl0.fill();
 
 // Flowers petalo
 
-let ctx2 = flowerDraw.getContext('2d');
+const fl1 = flowerDraw.getContext('2d');
 
-ctx2.beginPath();
-ctx2.arc(210,150,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
-
-// Flowers petalo
-
-let ctx3 = flowerDraw.getContext('2d');
-
-ctx3.beginPath();
-ctx3.arc(150,90,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
+fl1.beginPath();
+fl1.arc(90,150,20,0,2*Math.PI);
+fl1.fillStyle = 'purple';
+fl1.strokeStyle = "black";
+fl1.stroke();
+fl1.fill();
 
 
 // Flowers petalo
 
-let ctx4 = flowerDraw.getContext('2d');
+const fl2 = flowerDraw.getContext('2d');
 
-ctx4.beginPath();
-ctx4.arc(150,210,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
-
-// Flowers petalo
-
-let ctx5 = flowerDraw.getContext('2d');
-
-ctx5.beginPath();
-ctx5.arc(107,107,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
-
+fl2.beginPath();
+fl2.arc(210,150,20,0,2*Math.PI);
+fl2.fillStyle = 'purple';
+fl2.strokeStyle = "black";
+fl2.stroke();
+fl2.fill();
 
 // Flowers petalo
 
-let ctx6 = flowerDraw.getContext('2d');
+const fl3 = flowerDraw.getContext('2d');
 
-ctx6.beginPath();
-ctx6.arc(193,105,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
-
-// Flowers petalo
-
-let ctx7 = flowerDraw.getContext('2d');
-
-ctx7.beginPath();
-ctx7.arc(193,193,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
+fl3.beginPath();
+fl3.arc(150,90,20,0,2*Math.PI);
+fl3.fillStyle = 'purple';
+fl3.strokeStyle = "black";
+fl3.stroke();
+fl3.fill();
 
 
 // Flowers petalo
 
-let ctx8 = flowerDraw.getContext('2d');
+const fl4 = flowerDraw.getContext('2d');
 
-ctx8.beginPath();
-ctx8.arc(107,193,20,0,2*Math.PI);
-ctx.fillStyle = 'purple'
-ctx1.strokeStyle = "black";
-ctx1.stroke();
-ctx.fill();
+fl4.beginPath();
+fl4.arc(150,210,20,0,2*Math.PI);
+fl4.fillStyle = 'purple';
+fl4.strokeStyle = "black";
+fl4.stroke();
+fl4.fill();
+
+// Flowers petalo
+
+const fl5 = flowerDraw.getContext('2d');
+
+fl5.beginPath();
+fl5.arc(107,107,20,0,2*Math.PI);
+fl5.fillStyle = 'purple';
+fl5.strokeStyle = "black";
+fl5.stroke();
+fl5.fill();
+
+
+// Flowers petalo
+
+const fl6 = flowerDraw.getContext('2d');
+
+fl6.beginPath();
+fl6.arc(193,105,20,0,2*Math.PI);
+fl6.fillStyle = 'purple';
+fl6.strokeStyle = "black";
+fl6.stroke();
+fl6.fill();
+
+// Flowers petalo
+
+const fl7 = flowerDraw.getContext('2d');
+
+fl7.beginPath();
+fl7.arc(193,193,20,0,2*Math.PI);
+fl7.fillStyle = 'purple';
+fl7.strokeStyle = "black";
+fl7.stroke();
+fl7.fill();
+
+
+// Flowers petalo
+
+const fl8 = flowerDraw.getContext('2d');
+
+fl8.beginPath();
+fl8.arc(107,193,20,0,2*Math.PI);
+fl8.fillStyle = 'purple';
+fl8.strokeStyle = "black";
+fl8.stroke();
+fl8.fill();
 
 // Flowers branch
 
-let ctxBranch = flowerDraw.getContext('2d');
+const flBranch = flowerDraw.getContext('2d');
 
-ctxBranch.beginPath();
-ctxBranch.moveTo(150, 230);
-ctx.lineTo(150, 450);
-// ctxBranch.quadraticCurveTo(25, 25, 25, 300);
-ctxBranch.strokeStyle = "black";
-ctxBranch.stroke();
+flBranch.beginPath();
+flBranch.moveTo(150, 231);
+flBranch.lineTo(150, 450);
+flBranch.strokeStyle = "rgb(16, 53, 16)";
+fl0.lineWidth = 5;
+flBranch.stroke();
+
+
+
+// -----------------------------------------------------
+//  Create the Houses elements...
+
+const HouseDiv = document.createElement('div');
+HouseDiv.id = 'firstHouseElement';
+HouseDiv.innerHTML = `<canvas id='firstHouserDraw' width="100px" height="110px" style="background-color: " ></canvas>`;
+grownd.insertAdjacentElement('beforeend', HouseDiv);
+
+const firstHouse = document.getElementById('firstHouseElement');
+
+const firstHouseStyle = {
+    width: '100px',
+    height: '110px',
+    backgroundColor: '',
+    position: 'absolute',
+    left:'20%',
+    top:'0%',
+    transform: 'translate(-50%, -75%)'
+}
+
+Object.assign(firstHouse.style, firstHouseStyle);
+
+// --------------------------------------------------------
+//  Create the actual House using CANVAS
+
+const firstHouserDraw = document.getElementById('firstHouserDraw');
+
+
+const roof1 = firstHouserDraw.getContext('2d');
+
+roof1.beginPath();
+roof1.moveTo(100, 50);
+roof1.lineTo(50, 0);
+roof1.lineTo(0, 50);
+roof1.lineTo(100, 50);
+roof1.fillStyle = "rgb(65, 45, 45)";
+roof1.fill();
+roof1.strokeStyle = "black";
+roof1.stroke();
+
+
+const house = firstHouserDraw.getContext('2d');
+
+house.beginPath();
+house.moveTo(80, 110);
+house.lineTo(80, 50);
+house.lineTo(20, 50);
+house.lineTo(20, 110);
+house.lineTo(80, 110);
+house.fillStyle = "rgb(133, 93, 93";
+house.fill();
+house.strokeStyle = "black";
+house.stroke();
+
+
+
+
+
+
 
 
 
